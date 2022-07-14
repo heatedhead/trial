a55b5cea2ace8f6bc1ca2f2d4a345625df8ef154
Anil Raj <Aniljmjr@gmail.com>
2022-07-14 21:47:27 +0530

    commit

--- lib/_trial.dart
+++ lib/_trial.dart
@@ -1,4 +1,7 @@
 void main(){
-  List studentName=["Ajay","Anjali","Ardra","Anil"];
-  print(studentName);
-}
+  // List studentName=["Ajay","Anjali","Ardra","Anil"];
+  // print(studentName);
+  // var vegetable=List.filled(5,0);
+  // print(vegetable);
+final List<int>carName=List.filled(5,0);
+print(carName);}
